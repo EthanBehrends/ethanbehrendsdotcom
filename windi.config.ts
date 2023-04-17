94c2b9b40c87a0a532bfddfdb9405151edf9e930
EthanBehrends <epbehrends@gmail.com>
2023-04-16 23:34:52 -0500

    Is windi broken??

--- windi.config.ts
+++ windi.config.ts
@@ -13,26 +13,6 @@ export default defineConfig({
     },
   },
   plugins: [
-    plugin(({ addDynamic }) => {
-      addDynamic("l-", ({ Utility, Property, Style }) => {
-        const value = Utility.handler.handleNumber(1, 100, "int").value
-
-        if (value) {
-          return Style.generate(Utility.class, {
-            "--primary-l": `${value}%`
-          })
-        }
-      })
-      addDynamic("a-", ({ Utility, Property, Style }) => {
-        const value = Utility.handler.handleNumber(1, 100, "int").value
-
-        if (value) {
-          return Style.generate(Utility.class, {
-            "--primary-a": `${Number(value) / 100}`
-          })
-        }
-      })
-    }),
     interactionVaraints
   ]
 })
